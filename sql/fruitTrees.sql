e9dab5261b723c89535401f70a50978af47284a1
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-16 08:16:21 -0600

    updated the profile table

--- sql/fruitTrees.sql
+++ sql/fruitTrees.sql
@@ -5,7 +5,13 @@ DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS vote;
 
 CREATE TABLE IF NOT EXISTS profile (
-
+    profile_id UUID NOT NULL PRIMARY KEY,
+    profile_activation_token CHAR(96) NOT NULL,
+    profile_email VARCHAR (128) NOT NULL,
+    profile_hash CHAR (97) NOT NULL,
+    profile_join_date DATE NOT NULL,
+    profile_name VARCHAR (96) NOT NULL,
+    UNIQUE(profile_email)
 );
 
 CREATE TABLE IF NOT EXISTS tree (
