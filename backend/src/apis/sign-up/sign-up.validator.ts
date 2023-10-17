2c4cf329221f2685a2f3fbaaf818220fd5b92da1
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-17 15:46:03 -0600

    added sign-up validator and profile_image to sql

---
+++ backend/src/apis/sign-up/sign-up.validator.ts
@@ -0,0 +1,18 @@
+import {z} from 'zod';
+import {PrivateProfileSchema} from "../profile/profile.validator";
+
+export const SignUpProfileSchema = PrivateProfileSchema
+  .omit ({profileId: true, profileActivationToken: true, profileHash: true, profileImageUrl: true})
+  .extend({
+      profilePasswordConfirm: z.string()
+          .min(8, {message: 'please provide a valid password (min 8 characters)'})
+          .max(32, {message: 'please provide a valid password (max 32 characters)'}),
+      profilePassword: z.string()
+          .min(8,{message: 'please provide a valid password (min 8 characters)' })
+          .max(32, {message: 'please provide a valid password (max 32 characters)' })
+  })
+
+.refine(data => data.profilePassword === data.profilePasswordConfirm, {
+    message: 'passwords do not match'
+})
+
