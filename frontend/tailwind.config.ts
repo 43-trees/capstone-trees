a0eb36fa1d70ac420c5d3da245fe13d7ddacc588
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-30 15:06:18 -0600

    updated tailwind config with daisyui theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,7 +6,32 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#D1E2C4",
+
+          "secondary": "#8f4263",
+
+          "accent": "#5c7f67",
+
+          "neutral": "#291e00",
+
+          "base-100": "#EBEBE8",
+
+          "info": "#3abff8",
+
+          "success": "#36d399",
+
+          "warning": "#fbbd23",
+
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
