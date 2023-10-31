f70851915feea97f2f50b85f2b09ed1627495331
Rashaan Marrow <rmarrow@cnm.edu>
2023-10-31 16:12:32 -0700

    added footer

--- frontend/src/app/components/Foot.tsx
+++ frontend/src/app/components/Foot.tsx
@@ -1,7 +1,30 @@
 export function Foot () {
-    return (
-        <>
-        <h1>Footer</h1>
-        </>
+    const footerStyle = {
+        backgroundColor: '#000000',
+        color: '#F8F8FF',
+        padding: '20px 0',
+      //  position: 'fixed',
+        width: '100%',
+        bottom: '0',
+    };
+
+    const containerStyle = {
+        maxWidth: '1200px',
+        margin: '0 auto',
+    };
+
+    const pStyle = {
+        margin: 0,
+    };
+
+
+        return (
+            <footer style={footerStyle}>
+                <div style={containerStyle}>
+                    <p style={pStyle}>&copy; trees 2023</p>
+                </div>
+            </footer>
+
+
     )
 }
