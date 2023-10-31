0e9d24c0fbc0e56f3f72adabcaccf37f6135affe
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-31 13:50:39 -0600

    changes to components structure

--- frontend/src/app/components/info.tsx
+++ frontend/src/app/components/info.tsx
@@ -7,9 +7,9 @@ type InfoProps = {
 export function InfoBox(infoProps: InfoProps) {
     const {header, text} = infoProps
     return (
-        <section className="bg-primary mx-54">
+        <section className="bg-primary p-4">
             <h1 className="text-3xl">{header}</h1>
-            <p>{text}</p>
+            <p className="p-6">{text}</p>
         </section>
     )
 }
