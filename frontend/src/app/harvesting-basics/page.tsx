a5e1b54421bc37ce0af9971985d1e8698e4b23af
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-31 15:18:03 -0600

    created resources and harvesting 101 pages including adding necessary copy

---
+++ frontend/src/app/harvesting-basics/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function HarvestingBasics() {
+    return (
+        <>
+            <section className="bg-primary p-6 mx-56 my-12 drop-shadow-lg">
+                <h1 className="text-3xl">Harvesting 101</h1>
+                <div className="p-6">
+                    <p>Urban harvesting is a great way to source food directly from your local community, limiting the impact of the supply chain on the environment and reducing food waste. If you’re new to harvesting, or you need a reminder, here’s some tips to make the most of your efforts while practicing care for others: </p><br/>
+                    <ul className="px-20 list-disc leading-relaxed">
+                        <li><span className="font-bold">Respect nature:</span> Though trees are resilient, please be gentle while picking or cutting off fruit. Do not mark the trees or try to dig out roots. If you’re experienced in gardening, take care of the fruit trees with watering or trimming, but please leave them be if you’re unsure what they need. </li>
+                        <li><span className="font-bold">Respect the community:</span> Urban harvesting is a tool meant for anybody to utilize, remember that we are sharing with the whole community, including animals who rely on plants and fruit trees as their primary food source. Take what you need or will use and leave the rest for your human and non-human neighbors.</li>
+                        <li><span className="font-bold">Bring the right tools:</span> Have a basket or bag to store your harvest handy. You may want to wear gloves or use a handheld garden pruner to protect your hands and the tree. If you’ll be exploring outside for a while, don’t forget sunscreen and some water!</li>
+                        <li><span className="font-bold">Spread the love:</span> Try planting your own trees to share, organizing a community garden, or giving spare harvest to community organizations or a neighbor in need. </li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
