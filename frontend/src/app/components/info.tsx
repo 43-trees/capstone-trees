488893cf1726e002a8497fc7f3f22d3390d16b87
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-31 13:46:18 -0600

    changed global structure

--- frontend/src/app/components/info.tsx
+++ frontend/src/app/components/info.tsx
@@ -1,12 +1,14 @@
 type InfoProps = {
+    header: string
     text: string
 }
 
 
 export function InfoBox(infoProps: InfoProps) {
-    const {text} = infoProps
+    const {header, text} = infoProps
     return (
         <section className="bg-primary mx-54">
+            <h1 className="text-3xl">{header}</h1>
             <p>{text}</p>
         </section>
     )
