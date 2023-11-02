43676518ffc0c0b214e0b58f4e15d12129b71f90
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-02 17:00:20 -0600

    updated comment component

--- frontend/src/app/components/CommentPost.tsx
+++ frontend/src/app/components/CommentPost.tsx
@@ -1,16 +1,17 @@
 
 
 type CommentProps = {
-
+    commentContent: string
+    profileName: string
 }
 
 export function Comment(commentProps: CommentProps) {
-    const {} = commentProps
+    const {commentContent, profileName} = commentProps
     return (
         <>
-            <section className="bg-base-100">
-
-
+            <section className="bg-base-100 p-4 rounded-lg md:w-96 mx-auto">
+                <h3 className="text-md text-start font-semibold text-secondary">{profileName}</h3>
+                <p className="text-justify">{commentContent}</p>
             </section>
         </>
     )
