a9bca6d9a404ab8211d53a0adb884fcfa7159995
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-02 15:58:47 -0600

    started on comment component

---
+++ frontend/src/app/components/CommentPost.tsx
@@ -0,0 +1,15 @@
+
+type CommentProps = {
+
+}
+
+export function Comment(commentProps: CommentProps) {
+    const {} = commentProps
+    return (
+        <>
+            <section className="bg-base-100">
+
+            </section>
+        </>
+    )
+}
