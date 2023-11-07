b520f28f34052093516c5119b59ba3f64b54cfba
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-06 17:05:24 -0700

    added component for comment submit to work on tomorrow

---
+++ frontend/src/app/components/CommentSubmit.tsx
@@ -0,0 +1,18 @@
+
+
+type CommentSubmitProps = {
+    commentContent: string
+    profileName: string
+}
+
+export function Comment(commentProps: CommentSubmitProps) {
+    const {commentContent, profileName} = commentProps
+    return (
+        <>
+            <section className="bg-base-100 p-4 rounded-lg md:w-96 mx-auto">
+                <h3 className="text-md text-start font-semibold text-secondary">{profileName}</h3>
+                <p className="text-justify">{commentContent}</p>
+            </section>
+        </>
+    )
+}
