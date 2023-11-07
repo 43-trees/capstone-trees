2ecea6fab2317dcaa2ae50a0feae95fc9803ba10
Rashaan Marrow <rmarrow@cnm.edu>
2023-11-07 16:03:41 -0700

    added App.tsx and updated CommentSubmit.tsx

--- frontend/src/app/components/CommentSubmit.tsx
+++ frontend/src/app/components/CommentSubmit.tsx
@@ -1,17 +1,35 @@
-
+import React, {useState} from "react";
 
 type CommentSubmitProps = {
     commentContent: string
     profileName: string
-}
+    onComment: (comment: string) => void;
+};
 
 export function Comment(commentProps: CommentSubmitProps) {
-    const {commentContent, profileName} = commentProps
+    const { profileName, onComment } = commentProps;
+    const [comment, setComment] = useState('');
+
+    const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>)=> {
+        setComment(event.target.value);
+    };
+
+    const handleCommentSubmit=()=> {
+        onComment(comment);
+        setComment("");
+    };
+
     return (
         <>
             <section className="bg-base-100 p-4 rounded-lg md:w-96 mx-auto">
                 <h3 className="text-md text-start font-semibold text-secondary">{profileName}</h3>
-                <p className="text-justify">{commentContent}</p>
+                <textarea
+                    placeholder="comment here"
+                    value={comment}
+                    onChange={handleCommentChange}
+                    className="text-justify">
+                </textarea>
+                <button onClick={handleCommentSubmit}>Comment</button>
             </section>
         </>
     )
