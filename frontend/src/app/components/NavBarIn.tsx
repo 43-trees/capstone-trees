eaf3c6dc48599b52695f5daf04ca2139d9cd5451
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-07 17:51:12 -0700

    navbar styling updates

--- frontend/src/app/components/NavBarIn.tsx
+++ frontend/src/app/components/NavBarIn.tsx
@@ -7,7 +7,7 @@ export function NavBarIn() {
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                    <input type="text" placeholder="Search" className="input input-bordered bg-primary w-24 md:w-auto" />
                 </div>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
