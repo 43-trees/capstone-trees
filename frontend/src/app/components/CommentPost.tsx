ec260289b7887a4339396535eac55bf1cb7c1a7a
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-08 17:22:24 -0700

    added get data for comments

--- frontend/src/app/components/CommentPost.tsx
+++ frontend/src/app/components/CommentPost.tsx
@@ -1,12 +1,17 @@
+'use server'
+import {Tree, TreeSchema} from "@/utils/models/trees";
+import {Comment, CommentSchema} from "@/utils/models/comments";
 
 
 type CommentProps = {
+    treeId: string
     commentContent: string
     profileName: string
 }
 
-export function Comment(commentProps: CommentProps) {
-    const {commentContent, profileName} = commentProps
+export async function Comment(commentProps: CommentProps) {
+    const {commentContent, profileName, treeId} = commentProps
+    const {tree, comments} = await getData(treeId)
     return (
         <>
             <section className="bg-base-100 p-4 rounded-lg md:w-96 mx-auto">
@@ -15,4 +20,36 @@ export function Comment(commentProps: CommentProps) {
             </section>
         </>
     )
+}
+
+async function getData(treeId: string): Promise<{tree: Tree, comments: Comment[]}> {
+    const url = `${process.env.REST_API_URL}/apis/tree/${treeId}`
+
+    const treeResult = await fetch(url)
+        .then(response => {
+
+            if (response.status === 200 || response.status === 304) {
+                return response.json()
+            }
+            throw new Error('retrieving data failed')
+        }).catch(error => {
+            console.error(error)
+        })
+
+    const tree = TreeSchema.parse(treeResult?.data)
+
+    const commentResult = await fetch(url)
+        .then(response => {
+
+            if (response.status === 200 || response.status === 304) {
+                return response.json()
+            }
+            throw new Error('retrieving data failed')
+        }).catch(error => {
+            console.error(error)
+        })
+
+    const comments = CommentSchema.array().parse(commentResult?.data)
+
+    return {tree, comments}
 }
