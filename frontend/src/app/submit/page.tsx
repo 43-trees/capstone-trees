91509f9219de96246eb531fb7498809936b802fe
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-11-12 20:24:15 -0700

    added tree submit page and component, testing filter

--- frontend/src/app/submit/page.tsx
+++ frontend/src/app/submit/page.tsx
@@ -1,199 +1,25 @@
-import {Tree, TreeSchema} from "@/utils/models/trees";
-import {Formik, FormikHelpers, FormikProps} from "formik";
-import {toFormikValidationSchema} from "zod-formik-adapter";
+// import {Tree, TreeSchema} from "@/utils/models/trees";
 import React from "react";
-import {Session} from "@/utils/models/fetchSession";
-import {DisplayStatus} from "@/app/components/displayStatus";
-import {useDropzone} from "react-dropzone";
+import {getSession, Session} from "@/utils/models/fetchSession";
+import {SubmitTreeComponent} from "@/app/components/SubmitTree";
+// import {SubmitTreeComponent} from "@/app/components/SubmitTree";
 
-type TreeSubmitComponentProps = {
-    session : Session|undefined
-    treeId: string
-}
-export function TreeSubmitComponent(props: TreeSubmitComponentProps) {
-    const {treeId, session} = props
+
+export default async function TreeSubmit() {
+    const session = await getSession()
 
     if(session === undefined) {
-        return <></>
+
+        return <>
+            <p>poop</p>
+        </>
     }
 
-    const {profile, authorization} = session
-
-    const initialValues: any = {
-        treeId: treeId,
-        treeProfileId: '',
-        treeAddress: '',
-        treeEndDate: null,
-        treeDate: null,
-        treeImage: null,
-        treeInfo: '',
-        treeLat: null,
-        treeLng: null,
-        treeTitle: '',
-        treeSpecies: '',
-        imageUrl: ''
-    }
-
-    const handleSubmit = (values: Tree, actions: FormikHelpers<Tree>)=> {
-        const {setStatus, resetForm} = actions
-        fetch('/apis/tree', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "authorization": `${authorization}`
-            },
-            body: JSON.stringify(values)
-        })
-            .then(response => response.json())
-            .then(json => {
-            if(json.status === 200){
-                resetForm()
-            }
-            setStatus({type: json.type, message: json.message})
-        })
-    };
-
     return (
         <>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={handleSubmit}
-                validatorSchema={toFormikValidationSchema(TreeSchema)}
-            >
-                {SubmitTreeContent}
-            </Formik>
-        </>
-    )
-}
-
-
- function SubmitTreeContent(props: FormikProps<Tree>) {
-    const {
-        status,
-        values,
-        errors,
-        touched,
-        dirty,
-        isSubmitting,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        handleReset,
-        setFieldValue
-    } = props
-
-    return (
-        <>
-            <form  className="md:w-1/2 md:auto mx-auto grid-cols-1 auto-rows-max gap-6 mt-8">
-                <div>
-                    <h2 className="text-3xl text-center text-neutral/80 font-semibold p-2">Submit a Tree</h2>
-                </div>
-                <div className="py-3 dropdown flex justify-center">
-                    <label tabIndex={0} className="btn bg-secondary text-white m-1">Species</label>
-                    <select tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-                            value={values.treeSpecies}
-                            onBlur={handleBlur}
-                            onChange={handleChange}>
-                        <option value={''}>Filter by Species</option>
-                        <option value={'Apple'}>Apple</option>
-                        <option value={'Apricot'}>Apricot</option>
-                        <option value={'Cherry'}>Cherry</option>
-                        <option value={'Fig'}>Fig</option>
-                        <option value={'Peach'}>Peach</option>
-                        <option value={'Pear'}>Pear</option>
-                        <option value={'Plum'}>Plum</option>
-                        <option value={'Pomegranate'}>Pomegranate</option>
-                        <option value={'Other'}>Other</option>
-                    </select>
-                </div>
-                <div className="py-3">
-                    <label htmlFor="title" className="block text-gray text-sm font-bold mb-2">Title</label>
-                    <input type="text" id="title" name="title"
-                           className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray focus:bg-white focus:ring-0"
-                           onBlur={handleBlur}
-                           onChange={handleChange}
-                           value={values.treeTitle}
-                           />
-                </div>
-                <div className="py-3">
-                    <label htmlFor="address" className="block text-gray text-sm font-bold mb-2">Address</label>
-                    <input type="text" id="address" name="address"
-                           className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray focus:bg-white focus:ring-0"
-                           onBlur={handleBlur}
-                           onChange={handleChange}
-                           value={values.treeAddress}/>
-                </div>
-                <div className="py-3">
-                    <label htmlFor="info" className="block text-gray text-sm font-bold mb-2">Info</label>
-                    <input type="text" id="info" name="info"
-                           className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray focus:bg-white focus:ring-0"
-                           onBlur={handleBlur}
-                           onChange={handleChange}
-                           value={values.treeInfo}/>
-                </div>
-                <div>
-                    <ImageDropZone
-                        formikProps={{
-                            values,
-                            handleChange,
-                            handleBlur,
-                            setFieldValue,
-                            fieldValue: 'imageTreeId'
-                        }}
-                    />
-                <DisplayStatus status={status} />
-                </div>
-                <div className="py-3">
-                    <button type="submit" className="bg-secondary hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-                        Submit
-                    </button>
-                </div>
-            </form>
-
-        </>
-    )
-}
-
-function ImageDropZone ({ formikProps }: any) {
-
-    const onDrop = React.useCallback((acceptedFiles: any) => {
-
-        const formData = new FormData()
-        formData.append('image', acceptedFiles[0])
-
-        formikProps.setFieldValue(formikProps.fieldValue, formData)
-
-    }, [formikProps])
-    const { getInputProps, isDragActive } = useDropzone({ onDrop })
-
-    return (
-        <>
-            <label>Tree Images</label>
-                {
-                    formikProps.values.imageUrl &&
-                    <>
-                        <div className="bg-transparent m-0">
-                            <img  height={100}  width={100} alt="new tree image" src={formikProps.values.imageUrl} />
-                        </div>
-
-                    </>
-                }
-                <div className="d-flex flex-fill bg-light justify-content-center align-items-center border rounded">
-                    <input
-                        aria-label="profile avatar file drag and drop area"
-                        aria-describedby="image drag drop area"
-                        className="form-control-file"
-                        accept="image/*"
-                        onChange={formikProps.handleChange}
-                        onBlur={formikProps.handleBlur}
-                        {...getInputProps()}
-                    />
-                    {
-                        isDragActive ?
-                            <span className="align-items-center" >Drop image here</span> :
-                            <span className="align-items-center" >Drag and drop image here, or click here to select an image</span>
-                    }
-                </div>
+            <div className="">
+                <SubmitTreeComponent session={session}/>
+            </div>
         </>
     )
 }
